201cb63fdf5a41b3a91095e1c4fa9d4a579f3103
232524z <ianhaile@me.com>
2022-04-27 18:11:51 -0400

    Working L calculator

--- L_Dominoes.py
+++ L_Dominoes.py
@@ -1,9 +1,72 @@
+import numpy as np
 class L:
-    state = None
+    table = None
 
     def __init__(self, dimx, dimy):
-        self.state = [[0 for i in range(dimx)] for j in range(dimy)]
+        self.table = np.array([[0 for i in range(dimx)] for j in range(dimy)])
 
     def __repr__(self):
-        for row in self.state:
-            print(row)
+        return str(self.table)
+
+    @staticmethod
+    def mex(options):
+        for i in range(len(options)):
+            try:
+                options.index(i)
+            except ValueError:
+                return i
+        return len(options)+1
+
+    @staticmethod
+    def nim_sum(nim1, nim2):
+        nim1, nim2 = bin(nim1)[2:], bin(nim2)[2:]
+        sum = 0
+        length = max(len(nim1), len(nim2))
+        for i in range(1,length+1):
+            try:
+                digit1 = int(nim1[-i])
+            except:
+                digit1 = 0
+
+            try:
+                digit2 = int(nim2[-i])
+            except:
+                digit2 = 0
+
+
+            if digit1 != digit2:
+                sum += 2**(i-1)
+        return sum
+
+    def get_table(self):
+        return self.table
+
+    def set_value(self, row, col, value):
+        row, col = row-1, col-1
+        self.table[row][col] = value
+        try:
+            self.table[col][row] = value
+        except:
+            pass
+
+    def get_value(self, row, col):
+        if row == 0 or col == 0:
+            return 0
+        return self.table[row-1][col-1]
+
+
+    def find_options(self, height, width, index):
+        if index+1 < height:
+            up = self.get_value(width, height-(index+1))
+            down = self.nim_sum(index, width-1)
+            sideways = self.nim_sum(index, up)
+            options = [up,down,sideways]
+            return options
+        if index+1 == height:
+            up = width-1
+            right = height-1
+            out = self.nim_sum(width-1,height-1)
+            options = [up,right,out]
+            return options
+        if index +1 > height:
+            return self.find_options(width,height,((width+height-1)-index))
--- Main.py
+++ Main.py
@@ -1,3 +1,20 @@
+import numpy as np
 from L_Dominoes import L
-test = L(10, 10)
+table_size = 10
+test = L(table_size, table_size)
+
+
+
+for i in range(1,table_size+1):
+    test.set_value(1,i,i)
+    test.set_value(2, i, i + 1)
+
+for i in range(3,table_size+1):
+    for j in range(i,table_size+1):
+        options = []
+        for k in range(i+j-1):
+            options += test.find_options(i, j, k)
+        l_value = test.mex(options)
+        test.set_value(i, j, l_value)
+
 print(test)
